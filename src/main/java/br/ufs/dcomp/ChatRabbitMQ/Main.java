2862c13ad5ec3f716818d3835d7c62cde82f3e6b
Gabriel Teixeira <gabriel.silveira.dev@gmail.com>
2025-01-29 21:12:48 -0300

    Submissão da Etapa 1

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Main.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Main.java
@@ -7,20 +7,22 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
 public class Main {
-    private static final String HOST = "54.174.110.248";
+    private static final String HOST = "100.26.100.102";
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "password";
-    private static final String PROMPT = ">>";
+    private static final String PROMPT = ">> ";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         Connection connection = Chat.iniciarConexao(HOST, USERNAME, PASSWORD);
         Channel channel = Chat.iniciarCanal(connection);
 
         iniciarChat(channel);
+        channel.close();
+        connection.close();
     }
 
     public static void iniciarChat(Channel channel) throws IOException {
-        System.out.println("User: ");
+        System.out.print("User: ");
         String nomeUsuario = InputOutput.lerLinha();
 
         channel.queueDeclare(nomeUsuario, false, false, false, null);
@@ -31,16 +33,19 @@ public class Main {
         channel.basicConsume(nomeUsuario, true, consumer);
         String nomeDestinatario = "";
         while (true) {
-            System.out.println(nomeDestinatario.concat(PROMPT));
+            System.out.print(nomeDestinatario.concat(PROMPT));
             String entrada = InputOutput.lerLinha();
             if (nomeDestinatario.isBlank() || InputOutput.isUsuario(entrada)) {
                 nomeDestinatario = entrada;
                 channel.queueDeclare(nomeDestinatario.substring(1), false, false, false, null);
-//                channel.basicConsume(nomeDestinatario, true, consumer); Isso está certo? definir um consumidor pra cada destinatário de resposta?
+                System.out.println("entrou no loop");
+                channel.basicConsume(nomeDestinatario, true, consumer);
                 continue;
             }
             String mensagem = entrada;
             System.out.println(mensagem);
+            if(mensagem.equals("exit") || mensagem.equals("sair"))
+                break;
             System.out.println(nomeDestinatario);
             channel.basicPublish("", nomeDestinatario.substring(1), null, mensagem.getBytes(StandardCharsets.UTF_8));
         }
