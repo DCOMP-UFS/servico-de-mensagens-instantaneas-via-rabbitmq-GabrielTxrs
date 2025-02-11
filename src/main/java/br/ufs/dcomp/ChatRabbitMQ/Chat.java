65925debb909ea0aa628e2d4e20948d7a5db055c
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2025-02-10 22:14:50 -0300

    Iniciei a Etapa 2

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -24,24 +24,41 @@ public class Chat {
             throw new RuntimeException(e);
         }
     }
+
     public static void criarGrupo(String nomeGrupo) {
-        System.out.println("Grupo Criado: "+nomeGrupo);
+        System.out.println("Grupo Criado: " + nomeGrupo);
     }
 
     public static void deletarGrupo(String nomeGrupo) {
-        System.out.println("Grupo Deletado: "+nomeGrupo);
+        System.out.println("Grupo Deletado: " + nomeGrupo);
     }
 
     public static void removerUsuarioGrupo(String usuario, String nomeGrupo) {
-        System.out.println("Usuario: "+usuario+" removido do grupo: "+nomeGrupo);
+        System.out.println("Usuario: " + usuario + " removido do grupo: " + nomeGrupo);
     }
 
     public static void enviarMensagem(Channel channel, MensagemOuterClass.Mensagem mensagem) {
-//        if(destinatario == null || destinatario.isBlank() || mensagem == null || mensagem.isBlank()) {
-//            System.out.println("Não foi possível enviar a mensagem");
-//            return;
-//        }
-//        System.out.println("Destinatario: "+destinatario+" mensagem: "+mensagem);
+        if (!isMensagemValida(mensagem)) {
+            System.out.println("Não foi possível enviar a mensagem");
+            return;
+        }
+        try {
+            channel.basicPublish("", mensagem.getGrupo().substring(1), null, mensagem.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isMensagemValida(MensagemOuterClass.Mensagem mensagem) {
+        return mensagem.hasConteudo() && !mensagem.getConteudo().getNome().isBlank()
+                && !mensagem.getConteudo().getTipo().isBlank() && !mensagem.getConteudo().getCorpo().isEmpty()
+                && mensagem.hasField(MensagemOuterClass.Mensagem.getDescriptor().findFieldByName("emissor"))
+                && mensagem.hasField(MensagemOuterClass.Mensagem.getDescriptor().findFieldByName("data"))
+                && mensagem.hasField(MensagemOuterClass.Mensagem.getDescriptor().findFieldByName("hora"))
+                && mensagem.hasField(MensagemOuterClass.Mensagem.getDescriptor().findFieldByName("grupo"))
+                && mensagem.hasField(MensagemOuterClass.Conteudo.getDescriptor().findFieldByName("tipo"))
+                && mensagem.hasField(MensagemOuterClass.Conteudo.getDescriptor().findFieldByName("corpo"))
+                && mensagem.hasField(MensagemOuterClass.Conteudo.getDescriptor().findFieldByName("nome"));
     }
 }
 
--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Main.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Main.java
@@ -4,8 +4,12 @@ import com.google.protobuf.ByteString;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Date;
 import java.util.concurrent.TimeoutException;
 
 public class Main {
@@ -30,11 +34,17 @@ public class Main {
 
         channel.queueDeclare(nomeUsuario, false, false, false, null);
         Consumer consumer = new DefaultConsumer(channel) {
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws UnsupportedEncodingException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                String sender = properties.getHeaders().get("sender").toString();
+                String timestamp = new SimpleDateFormat("dd/MM/yyyy 'às' HH:mm").format(new Date());
+                String group = envelope.getExchange().isEmpty() ? "" : "#" + envelope.getExchange();
+                System.out.println("(" + timestamp + ") " + sender + group + " diz: " + message);
             }
         };
         channel.basicConsume(nomeUsuario, true, consumer);
         String nomeDestinatario = "";
+
         while (true) {
             System.out.print(nomeDestinatario.concat(PROMPT));
             String entrada = InputOutput.lerLinha();
@@ -45,13 +55,10 @@ public class Main {
                 String[] parametros = entrada.split(" ");
                 nomeDestinatario = parametros[0];
                 channel.queueDeclare(nomeDestinatario.substring(1), false, false, false, null);
-//                channel.basicConsume(nomeDestinatario.substring(1), true, consumer);
+                channel.basicConsume(nomeDestinatario.substring(1), true, consumer);
             } else {
                 InputOutput.getComando(entrada);
                 if (!InputOutput.isComando(entrada)) {
-//                    String mensagem = entrada;
-//                    Chat.enviarMensagem(channel, , entrada);
-
                     MensagemOuterClass.Conteudo conteudo = MensagemOuterClass.Conteudo.newBuilder()
                             .setTipo(String.valueOf(MensagemOuterClass.Conteudo.TIPO_FIELD_NUMBER))
                             .setCorpo(ByteString.copyFromUtf8(entrada)).build();
@@ -62,8 +69,7 @@ public class Main {
                             .setHora(LocalTime.now().toString())
                             .setGrupo(nomeDestinatario)
                             .setConteudo(conteudo).build();
-
-                    channel.basicPublish("", nomeDestinatario.substring(1), null, mensagem.toByteArray());
+                    Chat.enviarMensagem(channel, mensagem);
                 }
             }
         }
